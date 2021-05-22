7417a0c4e5646a5aae23524f9b72bcb38ba01d0a
JiWoo Park <68318499+jpark16@users.noreply.github.com>
2021-05-21 23:24:48 -0400

    Add files via upload

---
+++ mq-x.py
@@ -0,0 +1,121 @@
+import bmpsensor
+import time
+import RPi.GPIO as GPIO
+import time
+import Adafruit_DHT
+
+
+# change these as desired - they're the pins connected from the
+# SPI port on the ADC to the Cobbler
+SPICLK = 11
+SPIMISO = 9
+SPIMOSI = 10
+SPICS = 8
+mq7_dpin = 26
+mq7_apin = 0
+mq2_dpin = 16
+mq2_apin = 1
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 4
+
+#port init
+def init():
+         GPIO.setwarnings(False)
+         GPIO.cleanup()                 #clean up at the end of your script
+         GPIO.setmode(GPIO.BCM)         #to specify whilch pin numbering system
+         # set up the SPI interface pins
+         GPIO.setup(SPIMOSI, GPIO.OUT)
+         GPIO.setup(SPIMISO, GPIO.IN)
+         GPIO.setup(SPICLK, GPIO.OUT)
+         GPIO.setup(SPICS, GPIO.OUT)
+         GPIO.setup(mq7_dpin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+         GPIO.setup(mq2_dpin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+
+#read SPI data from MCP3008(or MCP3204) chip,8 possible adc's (0 thru 7)
+def readadc(adcnum, clockpin, mosipin, misopin, cspin):
+        if ((adcnum > 7) or (adcnum < 0)):
+                return -1
+        GPIO.output(cspin, True)        
+
+        GPIO.output(clockpin, False)  # start clock low
+        GPIO.output(cspin, False)     # bring CS low
+
+        commandout = adcnum
+        commandout |= 0x18  # start bit + single-ended bit
+        commandout <<= 3    # we only need to send 5 bits here
+        for i in range(5):
+                if (commandout & 0x80):
+                        GPIO.output(mosipin, True)
+                else:
+                        GPIO.output(mosipin, False)
+                commandout <<= 1
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+
+        adcout = 0
+        # read in one empty bit, one null bit and 10 ADC bits
+        for i in range(12):
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+                adcout <<= 1
+                if (GPIO.input(misopin)):
+                        adcout |= 0x1
+
+        GPIO.output(cspin, True)
+
+        adcout >>= 1       # first bit is 'null' so drop it
+        return adcout
+#main ioop
+def main():
+         init()
+         print ('please wait...')
+         time.sleep(20)
+         while True:
+                  COlevel=readadc(mq2_apin, SPICLK, SPIMOSI, SPIMISO, SPICS)                  
+                  COlevel=readadc(mq7_apin, SPICLK, SPIMOSI, SPIMISO, SPICS)
+
+                  if GPIO.input(mq7_dpin):
+                           print("CO not leak")
+                           time.sleep(0.5)
+                  else:
+                           print("CO is detected")
+                           print("Current CO AD vaule = " +str("%.2f"%((COlevel/1024.)*5))+" V")
+                           print("Current CO density is:" +str("%.2f"%((COlevel/1024.)*100))+" %")
+                           time.sleep(0.5)
+                           
+                  if GPIO.input(mq2_dpin):
+                           print("Gas not leak")
+                           time.sleep(0.5)
+                  else:
+                           print("Gas leakage")
+                           print("Current Gas AD value = " +str("%.2f"%((COlevel/1024.)*3.3))+" V")
+                           time.sleep(0.5)
+                  
+                  #BMP180 setup
+                  temp, pressure, altitude = bmpsensor.readBmp180()
+                  print("\n")
+                  print("Temperature is ",temp)  # degC
+                  print("Pressure is ",pressure) # Pressure in Pa 
+                  print("Altitude is ",altitude) # Altitude in meters
+                  print("\n")
+                  time.sleep(2)
+                  
+                  #DHT11 setup
+                  humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+                  if humidity is not None and temperature is not None:
+                      print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
+                      print("\n")
+                  else:
+                      print("Sensor failure. Check wiring.");
+                      print("\n")
+                  time.sleep(2);
+
+
+if __name__ =='__main__':
+         try:
+                  main()
+                  pass
+         except KeyboardInterrupt:
+                  pass
+
+GPIO.cleanup()
